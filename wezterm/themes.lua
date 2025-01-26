64dcfea39186bedf2bafd388d6f9b24fa2e34cdd
Adrian Costin <adriancostin6@gmail.com>
2025-01-26 17:11:09 +0200

    modular theme selector

---
+++ wezterm/themes.lua
@@ -0,0 +1,50 @@
+local M = {}
+
+local wezterm = require'wezterm'
+
+local appearance = wezterm.gui.get_appearance()
+
+local themes = {
+    'Catppuccin Latte',
+    'Catppuccin Frappe',
+    'Catppuccin Macchiato',
+    'Catppuccin Mocha',
+}
+
+local function get_choices()
+  local choices = {}
+  for _, theme in ipairs(themes) do
+    table.insert(choices, { label = theme, id = theme})
+  end
+  return choices
+end
+
+local function switch(window, pane, id, label)
+  if not i and not label then
+    wezterm.log_error("Theme is undefined.")
+    return
+  end
+
+  local theme = label
+  window:set_config_overrides({ color_scheme = theme })
+end
+
+function M.select(window, pane)
+  local choices = get_choices()
+  window:perform_action(
+    wezterm.action.InputSelector({
+      action = wezterm.action_callback(switch),
+      title = 'Color schemes',
+      choices = choices,
+      fuzzy = true,
+      fuzzy_description = "Fuzzy select color scheme.",
+    }),
+    pane
+  )
+end
+
+M.default = appearance:find('Dark') and 'Catppuccin Frappe' or 'Catppuccin Latte'
+M.delta = appearance:find('Dark') and 'delta-dark' or 'delta-light'
+M.system = appearance:find('Dark') and 'dark' or 'light'
+
+return M
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,44 +1,20 @@
 local wezterm = require'wezterm'
+
+local themes = require'themes'
+local binds = require'binds'
+
 -- https://wezfurlong.org/wezterm/config/lua/config/webgpu_preferred_adapter.html
 local gpus = wezterm.gui.enumerate_gpus()
-local theme = wezterm.gui.get_appearance()
+local system_theme = wezterm.gui.get_appearance()
 
-local themes = {
-    'Catppuccin Latte',
-    'Catppuccin Frappe',
-    'Catppuccin Macchiato',
-    'Catppuccin Mocha',
-}
-wezterm.on('spawn-theme-selector', function(window, pane)
-  local choices = {}
-  for _, theme in ipairs(themes) do
-    table.insert(choices, { label = theme, id = theme})
-  end
-  window:perform_action(
-    wezterm.action.InputSelector({
-      action = wezterm.action_callback(function(window, pane, id, label)
-        if not id and not label then
-            return
-        end
-        local theme = label
-        wezterm.log_info(theme)
-        window:set_config_overrides({ color_scheme = theme })
-      end),
-      title = 'Color schemes',
-      choices = choices,
-      fuzzy = true,
-      fuzzy_description = "Fuzzy select color scheme.",
-    }),
-    pane
-  )
-end)
+wezterm.on('spawn-theme-selector', themes.select)
 
 return {
 --  front_end = 'WebGpu',
 --  webgpu_preferred_adapter = gpus[1],
   font              = wezterm.font('Iosevka NF', {weight = 'Light',}),
   enable_tab_bar    = false,
-  color_scheme = theme:find('Dark') and 'Catppuccin Frappe' or 'Catppuccin Latte',
+  color_scheme = themes.default,
 
   window_padding = {
     top     = 0,
@@ -49,76 +25,9 @@ return {
 
   set_environment_variables = {
       EDITOR='nvim',
-
       DEV_HOME              = '~/repos',
-      SYSTEM_THEME          = theme:find('Dark') and 'dark' or 'light',
-      DELTA_FEATURES        = theme:find('Dark') and 'delta-dark' or 'delta-light',
+      SYSTEM_THEME          = themes.system,
+      DELTA_FEATURES        = themes.delta,
   },
-
-  keys = {
-    -- Tabs
-    {
-        key   = 'n',
-        mods  = 'META|SHIFT',
-        action = wezterm.action.SpawnTab 'CurrentPaneDomain',
-    },
-    {
-        key   = 'r',
-        mods  = 'META|SHIFT',
-        action = wezterm.action.ReloadConfiguration,
-    },
-    {
-        key   = 'c',
-        mods  = 'META|SHIFT',
-        action = wezterm.action.CloseCurrentTab { confirm = true },
-    },
-    {
-        key   = 'l',
-        mods  = 'META|SHIFT',
-        action = wezterm.action.ActivateTabRelative(1),
-    },
-    {
-        key   = 'h',
-        mods  = 'META|SHIFT',
-        action = wezterm.action.ActivateTabRelative(-1),
-    },
-
-    -- Scrollback
-    {
-        key   = 'j',
-        mods  = 'META|SHIFT',
-        action = wezterm.action.ScrollByLine(1),
-    },
-    {
-        key   = 'k',
-        mods  = 'META|SHIFT',
-        action = wezterm.action.ScrollByLine(-1),
-    },
-    {
-        key   = 'j',
-        mods  = 'SUPER|SHIFT',
-        action = wezterm.action.ScrollByPage(0.5),
-    },
-    {
-        key   = 'k',
-        mods  = 'SUPER|SHIFT',
-        action = wezterm.action.ScrollByPage(-0.5),
-    },
-
-    -- Fullscreen
-    {
-        key   = 'f',
-        mods  = 'META|SHIFT',
-        action = wezterm.action.ToggleFullScreen
-    },
-
-    {
-      key = 's',
-      mods  = 'META|SHIFT',
-      action = wezterm.action.EmitEvent("spawn-theme-selector")
-    },
-  },
-
-  -- TODO: Add key tables for Search Mode and Copy Mode to mak it more VI like
-
+  keys = binds,
 }
