1abeae96345c3471bfeff800aae04f3a279c7bf8
Adrian Costin <adriancostin6@gmail.com>
2025-06-01 14:57:33 +0300

    drop wezterm redundat config elements
    - Font will always fail, so use the default one.
    - WebGpu is preferred frontend, drop extra lines from config.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,18 +3,18 @@ local wezterm = require'wezterm'
 local themes = require'themes'
 local binds = require'binds'
 
--- https://wezfurlong.org/wezterm/config/lua/config/webgpu_preferred_adapter.html
-local gpus = wezterm.gui.enumerate_gpus()
-
+-- +--------+
+-- | events |
+-- +--------+-------------------------------------------------------------------
 wezterm.on('spawn-theme-selector', themes.select)
 
+-- +--------+
+-- | config |
+-- +--------+-------------------------------------------------------------------
 config = {}
 
---  front_end = 'WebGpu',
---  webgpu_preferred_adapter = gpus[1],
 config.prefer_egl = true
-config.font              = wezterm.font('Iosevka', {weight = 'Light',})
-config.enable_tab_bar    = false
+config.enable_tab_bar = false
 
 if type(themes.default) == "string" then
     config.color_scheme = themes.default
@@ -37,6 +37,7 @@ config.set_environment_variables = {
     SYSTEM_THEME          = themes.system,
     DELTA_FEATURES        = themes.delta,
 }
+
 config.keys = binds
 
 return config
