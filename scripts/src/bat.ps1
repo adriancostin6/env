7f8d15f6a47a4639dd9a8a7793bc64f7bd11488d
Adrian Costin <adriancostin6@gmail.com>
2025-09-17 12:37:15 +0300

    bat: cache theme to local filesystem for persistence

--- scripts/src/bat.ps1
+++ scripts/src/bat.ps1
@@ -1,20 +1,10 @@
-$bat_theme=Get-DefaultBatTheme
-if ($bat_theme == 'ansi') {
-    return
-}
-$env:BAT_THEME=$bat_theme
-
 if (-not (Get-Command -CommandType Application -ErrorAction SilentlyContinue bat)) {
     Write-Error "bat: command not found, please install it for pretty colors."
     return
 }
 
-$bat_config = bat --config-dir
-
-if (-not (Test-Path -Path $bat_config)) {
-    Write-Warning "bat: theme folder not found, creating."
-    New-Item -Path $themes -ItemType Directory -Force
-}
+$env:BatPs1Cache = "${env:PWSH_CFG_CACHE_DIR}/bat"
+New-Item -Force -ItemType Directory -Path $env:BatPs1Cache | Out-Null
 
 $Global:BatThemes = @(
     'Catppuccin Latte',
@@ -22,32 +12,53 @@ $Global:BatThemes = @(
     'Catppuccin Macchiato',
     'Catppuccin Mocha'
 )
-function Update-BatTheme {
-    param (
-        [switch] $BuildCache
-    )
-
-    if ($BuildCache) {
-        bat cache --build
-        Clear-Host
-    }
-
-    $theme = $Global:BatThemes | fzf
-    $env:BAT_THEME=$theme
-}
 
 function Get-DefaultBatTheme {
-    if (Test-Path -Path "${HOME}/.bat") {
-        $theme = Get-Content -Path "${HOME}/.bat"
+    [OutputType([string])]
+
+    [string] $themeFile = "${env:BatPs1Cache}/theme"
+    if (Test-Path -Path $themeFile) {
+        [string] $theme = Get-Content -Path $themeFile
         return $theme
     }
     return 'ansi'
 }
 
-if (-not (Test-Path -Path $bat_config\themes\*)) {
+function Update-BatTheme {
+    param (
+        [switch] $BuildBatCache
+    )
+
+    if ($BuildBatCache) {
+        bat cache --build
+    }
+
+    [string] $theme = $Global:BatThemes | fzf
+    $env:BAT_THEME=$theme
+
+    $env:BAT_THEME | Out-File -FilePath "${env:BatPs1Cache}/theme"
+    Write-Output "bat: default theme set to $env:BAT_THEME."
+}
+
+
+$env:BAT_THEME=Get-DefaultBatTheme
+
+[bool] $isSetupDone = $env:BAT_THEME -ne 'ansi'
+if ($isSetupDone) { # we already did the setup
+    return
+}
+
+[string] $batCfg = bat --config-dir
+
+if (-not (Test-Path -Path $batCfg)) {
+    Write-Warning "bat: theme folder not found, creating."
+    New-Item -Force -Path $themes -ItemType Directory | Out-Null
+}
+
+if (-not (Test-Path -Path $batCfg\themes\*)) {
     Write-Warning "bat: theme folder is empty, trying to download some themes."
 
-    $urls = @(
+    [string[]] $urls = @(
         "https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Latte.tmTheme",
         "https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Frappe.tmTheme",
         "https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Macchiato.tmTheme",
@@ -55,20 +66,17 @@ if (-not (Test-Path -Path $bat_config\themes\*)) {
     )
 
     try {
-        $i = 0;
+        [int] $i = 0;
         foreach ($url in $urls) {
-            $progress = ($i / $urls.Count) * 100
+            [int] $progress = ($i / $urls.Count) * 100
             Write-Progress -Activity "Downloading themes..." -PercentComplete $progress -Status $url
-            Invoke-WebRequest -ProgressAction SilentlyContinue -Uri $url -OutFile "${bat_config}/themes"
+            Invoke-WebRequest -ProgressAction SilentlyContinue -Uri $url -OutFile "${batCfg}/themes"
             $i = $i + 1
         }
     } catch [System.Net.WebException] {
         Write-Error "bat: theme folder is empty and no themes could be downloaded, have a look if you want pretty colors."
         return
     }
-
-    Update-BatTheme -BuildCache
-    $env:BAT_THEME | Out-File -FilePath "${HOME}/.bat"
-    Clear-Host
-    Write-Output "bat: default theme set to $env:BAT_THEME."
 }
+
+Update-BatTheme -BuildCache
--- scripts/src/init.ps1
+++ scripts/src/init.ps1
@@ -1,6 +1,9 @@
+. ./cache.ps1   # 1st
+
+# Tools
 . ./bat.ps1
 . ./eza.ps1
 . ./git.ps1
 . ./nvim.ps1
 
-. ./aliases.ps1
+. ./aliases.ps1 # last
