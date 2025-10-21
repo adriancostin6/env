d6ead6ea60a26d06c3ff873f498448c8e5dbcefa
Adrian Costin <adriancostin6@gmail.com>
2025-10-22 00:37:05 +0300

    pwsh: add deferred profile loading for time consuming tasks

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -2,4 +2,19 @@ Push-Location "$HOME/env/scripts"
 . ./init.ps1
 Pop-Location
 
-oh-my-posh --config "~/env/oh-my-posh/.adrianc.omp.json" init pwsh | Invoke-Expression
+# https://matt.kotsenas.com/posts/pwsh-profiling-async-startup
+[System.Collections.Queue] $asyncModules = @(
+    {
+        oh-my-posh --config "~/env/oh-my-posh/.adrianc.omp.json" init pwsh | Invoke-Expression
+        $env:OhMyPoshPs1Loaded = $true
+    }
+)
+Register-EngineEvent -SourceIdentifier PowerShell.OnIdle -SupportEvent -Action {
+    if ($asyncModules.Count -gt 0) {
+        & $asyncModules.Dequeue()
+    } else {
+        Unregister-Event -SubscriptionId $EventSubscriber.SubscriptionId -Force
+        Remove-Variable -Name 'asyncModules' -Scope Global -Force
+        [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+    }
+}
