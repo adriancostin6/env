cf60b128d47135a43050573f2688252c1f6e61fd
Adrian Costin <adriancostin6@gmail.com>
2025-10-23 14:53:31 +0200

    bash: add VI mode and disable buggy bash completions

--- scripts/src/completions.sh
+++ scripts/src/completions.sh
@@ -1,11 +1,2 @@
-# order is important. bash before fzf ^(.)^
-if ! shopt -oq posix; then
-	if [ -f /usr/share/bash-completion/bash_completion ]; then
-		. /usr/share/bash-completion/bash_completion
-	elif [ -f /etc/bash_completion ]; then
-		. /etc/bash_completion
-	fi
-fi
-
 # https://github.com/junegunn/fzf?tab=readme-ov-file#key-bindings-for-command-line 
 eval "$(fzf --bash)"
