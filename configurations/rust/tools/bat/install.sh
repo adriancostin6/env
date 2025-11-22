4b2a601288afd96c48feec5d16c3ec58f1b51630
Adrian Costin <adriancostin6@gmail.com>
2025-11-23 01:35:39 +0200

    add more logging to install scripts

--- configurations/rust/tools/bat/install.sh
+++ configurations/rust/tools/bat/install.sh
@@ -3,8 +3,13 @@
 source ../../env.sh
 source ../../utils.sh
 
-ENV_RUST_BAT_INSTALL_LOCK="$ENV_RUST_TOOLS_CACHE_DIR/bat-install.lock"
-env_rust_can_install_pkg "$ENV_RUST_BAT_INSTALL_LOCK" || exit 1
+logger_set_log_component "bat"
 
-cargo install --locked bat
+ENV_RUST_BAT_INSTALL_LOCK="$ENV_RUST_TOOLS_CACHE_DIR/bat-install.lock"
+env_rust_can_install_pkg "$ENV_RUST_BAT_INSTALL_LOCK" || die "Cannot install bat" | tee -a $ENV_RUST_TOOLS_STATE_DIR/bat.log
+
+log "installing bat." | tee -a $ENV_RUST_TOOLS_STATE_DIR/bat.log
+cargo install --locked bat &| tee -a $ENV_RUST_TOOLS_STATE_DIR/bat.log
+
+log "setup done, creating $ENV_RUST_BAT_INSTALL_LOCK." | tee -a $ENV_RUST_TOOLS_STATE_DIR/bat.log
 touch "$ENV_RUST_BAT_INSTALL_LOCK"
--- configurations/rust/tools/cargo-update/uninstall.sh
+++ configurations/rust/tools/cargo-update/uninstall.sh
@@ -3,8 +3,13 @@
 source ../../env.sh
 source ../../utils.sh
 
-ENV_RUST_CARGO_UPDATE_INSTALL_LOCK="$ENV_RUST_TOOLS_CACHE_DIR/cargo-update.install.lock"
-env_rust_can_uninstall_pkg "$ENV_RUST_CARGO_UPDATE_INSTALL_LOCK" || exit 1
+logger_set_log_component "cargo-update"
 
-cargo uninstall cargo_update
+ENV_RUST_CARGO_UPDATE_INSTALL_LOCK="$ENV_RUST_TOOLS_CACHE_DIR/cargo-update.install.lock"
+env_rust_can_uninstall_pkg "$ENV_RUST_CARGO_UPDATE_INSTALL_LOCK" || die "cannot install" | tee -a "$ENV_RUST_TOOLS_STATE_DIR/cargo-update.log"
+
+log "installing" | tee -a "$ENV_RUST_TOOLS_STATE_DIR/cargo-update.log"
+cargo uninstall cargo_update &| tee -a "$ENV_RUST_TOOLS_STATE_DIR/cargo-update.log"
+
+log "cleanup done, removing $ENV_RUST_CARGO_UPDATE_INSTALL_LOCK." | tee -a "$ENV_RUST_TOOLS_STATE_DIR/cargo-update.log"
 rm -f "$ENV_RUST_CARGO_UPDATE_INSTALL_LOCK"
